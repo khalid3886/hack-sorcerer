c35dfef979ca651822c6cf9719c822a9ca077ea4
PrityRastogi16 <surajyoti9839@gmail.com>
2024-02-26 01:23:09 +0530

    deployemnet-links

--- frontend/signup/script.js
+++ frontend/signup/script.js
@@ -149,7 +149,7 @@ document.getElementById('login-button').addEventListener('click',()=>{
             localStorage.setItem('token',data.access_token);
             localStorage.setItem('name',data.user.name);
             
-            window.location.href='../public/index.html'
+            location.href='../public/index.html'
             
         }
     })

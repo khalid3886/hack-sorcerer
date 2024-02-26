1334ff3931c4c7839c02df34d50050f254f33dde
PrityRastogi16 <surajyoti9839@gmail.com>
2024-02-26 10:46:47 +0530

    calender-done

--- frontend/public/js/landing.js
+++ frontend/public/js/landing.js
@@ -208,11 +208,14 @@ if(localStorage.getItem('userImage')) {
     //     }
     //   };
       // Example button click event
-    // document.querySelector('.logoutBTN').addEventListener("click", ()=>{
-    //     localStorage.removeItem('token');
-    //     localStorage.removeItem('email');
-    //     location.href = "./../signup/index.html";
+    document.querySelector('.logoutBTN').addEventListener("click", ()=>{
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+        location.href = "../index.html";
        
-    // }
-    // );
+    }
+    );
     
+    document.querySelector(".calenderIcon").addEventListener("click",()=>{
+        location.href = "./../calender/calender.html";
+    })
